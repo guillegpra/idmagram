8df0f2227a35ed7f7f386f9acaf0406f885e1d18
guillermogarciapradales <guillegpra@gmail.com>
2023-03-20 15:28:35 +0000

    Update database foreign keys

--- database.sql
+++ database.sql
@@ -2,21 +2,23 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 CREATE TABLE users (
-    id int(11) NOT NULL PRIMARY KEY,
+    id int(11) NOT NULL,
     username varchar(512) NOT NULL,
     email varchar(512) NOT NULL,
     pwd varchar(256) NOT NULL,
     first_name varchar(255) DEFAULT NULL,
-    last_name varchar(255) DEFAULT NULL
+    last_name varchar(255) DEFAULT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE photos (
-    id int(11) NOT NULL PRIMARY KEY,
+    id int(11) NOT NULL,
     caption varchar(512) DEFAULT NULL,
     user_id int(11) DEFAULT NULL,
   	number_likes int(11) NOT NULL DEFAULT '0',
     number_comments int(11) NOT NULL DEFAULT '0',
     date_upload datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -25,5 +27,8 @@ CREATE TABLE comments (
     user_id int(11) NOT NULL,
     photo_id int(11) NOT NULL,
     content text NOT NULL,
-    date_upload datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+    date_upload datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (photo_id) REFERENCES photos(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
