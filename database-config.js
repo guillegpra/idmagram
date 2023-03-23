0bc8c6f3a708d3f22c2e965ffcc5788f036fea09
guillermogarciapradales <guillegpra@gmail.com>
2023-03-23 20:41:46 +0000

    login functionality

---
+++ database-config.js
@@ -0,0 +1,27 @@
+const mysql = require("mysql");
+
+/* --------- Connection to database --------- */
+const connection = mysql.createConnection({
+    host: "localhost",
+    database: "image_sharing_app",
+    user: "root",
+    password: "your_new_password"
+});
+
+// const connection = mysql.createConnection({
+//     host: "mysql.scss.tcd.ie",
+//     database: "garcag_2223_db",
+//     user: "garcag",
+//     password: "ahFai3yu"
+// });
+
+connection.connect(function(err) {
+    if (err) {
+        console.log("Error connecting to database: " + err);
+    }
+    else {
+        console.log("Connected to database");
+    }
+}); 
+
+module.exports = { connection };
---
+++ passport-config.js
@@ -0,0 +1,47 @@
+const localStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const { connection } = require("./database-config");
+
+function initialize(passport, getUserByUsername, getUserById) {
+    const authenticateUser = async (username, password, done) => {
+
+        const query = 'SELECT * FROM users WHERE username = ?';
+        connection.query(query, [username], async (err, result) => {
+            if (err !== null) {
+                console.log("Error performing query: " + err);
+                return;
+            }
+
+            // Query performed correctly 
+            const user = result[0];
+
+            if (user === undefined) { // user doesn't exist
+                console.log("authentication error");
+                return done(null, false, { message: "The username doesn't exist" });
+            }
+    
+            try {
+                if (await bcrypt.compare(password, user.pwd)) {
+                    console.log("Logged in correctly");
+                    return done(null, user);
+                } 
+                else {
+                    console.log("Error logging in");
+                    return done(null, false, { message: "Password incorrect" });
+                }
+            } catch (error) {
+                return done(error);
+            }
+        }); 
+        
+    };
+
+    passport.use(new localStrategy(/* { usernameField: "username" }, */authenticateUser));
+    passport.serializeUser((user, done) => { done(null, user.id) });
+    passport.deserializeUser((id, done) => {
+        return done(null, getUserById(id));
+    });
+
+}
+
+module.exports = initialize;
