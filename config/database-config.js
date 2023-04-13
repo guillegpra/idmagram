598b8cf0521fd3b6e5768f864d80cbb7a64f5d11
guillermogarciapradales <guillegpra@gmail.com>
2023-04-13 22:56:24 +0100

    remove confidential info

--- config/database-config.js
+++ config/database-config.js
@@ -8,20 +8,6 @@ const connection = mysql.createConnection({
     password: "root"
 });
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     database: "image_sharing_app",
-//     user: "root",
-//     password: "your_new_password"
-// });
-
-// const connection = mysql.createConnection({
-//     host: "mysql.scss.tcd.ie",
-//     database: "garcag_2223_db",
-//     user: "garcag",
-//     password: "ahFai3yu"
-// });
-
 connection.connect(function(err) {
     if (err) {
         console.log("Error connecting to database: " + err);
